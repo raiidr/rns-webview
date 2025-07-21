bc000f2b8fbc4e7fdca5e4e74b595700dc6ba6a5
Invariant-tech-green <greengraphicx@invarianttechnologies.com>
2025-07-21 17:10:21 +0000

    Prepare version 1.0.4 with peerDependencies and access config

--- plugin/withAndroidWebView.js
+++ plugin/withAndroidWebView.js
@@ -1,25 +1,26 @@
 const { withDangerousMod } = require('@expo/config-plugins');
-const fs = require('fs');
+
 const path = require('path');
+const fs = require('fs-extra'); // use this for recursive copy
 
 module.exports = function (config, props = {}) {
-    return withDangerousMod(config, [
-        'android',
-        async (config) => {
-            const targetPath = path.join(config.modRequest.projectRoot, 'android', 'app', 'src', 'main', 'java', 'com', 'rnswebview');
-            const sourcePath = path.join(__dirname, '..', 'android');
+    return withDangerousMod(config, ['android', async config => {
+        const sourceDir = path.join(__dirname, '..', 'android', 'src');
+        const targetDir = path.join(
+            config.modRequest.projectRoot,
+            'android',
+            'app',
+            'src',
+            'main',
+            'java',
+            'com',
+            'rnswebview'
+        );
 
-            // Ensure the target directory exists
-            fs.mkdirSync(targetPath, { recursive: true });
+        await fs.ensureDir(targetDir);
+        await fs.copy(sourceDir, targetDir); // recursively copy everything
 
-            // Copy each Kotlin file from source to target
-            fs.readdirSync(sourcePath).forEach(file => {
-                const src = path.join(sourcePath, file);
-                const dest = path.join(targetPath, file);
-                fs.copyFileSync(src, dest);
-            });
-
-            return config;
-        },
-    ]);
+        return config;
+    }]);
 };
+
