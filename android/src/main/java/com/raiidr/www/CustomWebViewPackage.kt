197f6f0a70e3eb70b8a3ebd78d8fc0023381e07e
Invariant-tech-green <greengraphicx@invarianttechnologies.com>
2025-07-22 10:03:01 +0000

    New Update

--- android/src/main/java/com/raiidr/www/CustomWebViewPackage.kt
+++ android/src/main/java/com/raiidr/www/CustomWebViewPackage.kt
@@ -6,6 +6,8 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
+import com.facebook.react.ReactPackage
+
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 
