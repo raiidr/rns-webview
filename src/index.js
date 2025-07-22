fbb586de120ba31f5ebe377574fbd1d9720cc11f
Invariant-tech-green <greengraphicx@invarianttechnologies.com>
2025-07-22 07:19:44 +0000

    Initial update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import { requireNativeComponent } from "react-native";
-const RaiidrWebView = requireNativeComponent("MyRaiidrWebView");
-module.exports = RaiidrWebView
+// src/index.js
+import { requireNativeComponent, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const NativeWebView = requireNativeComponent('MyRaiidrWebView');
+
+export default function RaiidrWebView({
+    source,
+    suppressNavigationEvents = false,
+    descendantFocusability = 'afterDescendants',
+    style,
+    onNavigationStateChange,
+}) {
+    return (
+        <NativeWebView
+            source={source}
+            suppressNavigationEvents={suppressNavigationEvents}
+            descendantFocusability={descendantFocusability}
+            style={style}
+            onNavigationStateChange={onNavigationStateChange}
+        />
+    );
+}
+
+RaiidrWebView.propTypes = {
+    source: PropTypes.shape({
+        uri: PropTypes.string.isRequired,
+    }).isRequired,
+    suppressNavigationEvents: PropTypes.bool,
+    descendantFocusability: PropTypes.oneOf([
+        'blockDescendants',
+        'beforeDescendants',
+        'afterDescendants',
+    ]),
+    style: ViewPropTypes.style,
+    onNavigationStateChange: PropTypes.func,
+};
